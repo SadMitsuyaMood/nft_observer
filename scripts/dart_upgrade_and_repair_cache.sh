09967adc28ae8f4d17758180be222b00c03c5beb
Maksim Stankevich <maksim.st.work@gmail.com>
2024-05-20 20:12:07 +0200

    Update the script and analysis_options.yaml

--- scripts/dart_upgrade_and_repair_cache.sh
+++ scripts/dart_upgrade_and_repair_cache.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+echo "*** root packages ***"
 cd ..
-for folder in core
+for folder in core core_ui navigation
 do
     echo "*** $folder ***"
     cd "$folder"
     dart pub upgrade && dart pub cache repair && cd ..
 done
 
+echo "*** features ***"
+cd features
+for folder in splash
+do
+    echo "*** $folder ***"
+    cd "$folder"
+    dart pub upgrade && dart pub cache repair && cd ..
+done
+cd ..
+
+echo "*** root ***"
 dart pub upgrade && dart pub cache repair
